6757235da0b29d3574fbc05d359c51fcbff75aee
Sergey.Antonov <Sergey.Antonov@nexign-systems.com>
2019-12-27 16:36:15 +0300

    develop mobile version

--- app/mobile/components/Header/index.js
+++ app/mobile/components/Header/index.js
@@ -1,7 +1,106 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+import { observable, action } from 'mobx';
+import createStyles from '@material-ui/styles/createStyles/createStyles';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-export default function Header(){
-  return(
-    <div>header</div>
-  )
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import { Menu as AntMenu } from 'antd';
+
+import style from '../../../theme/style';
+import { BilegoIconMenuDotted, BilegoIconSearch } from '../../../theme/bilegoIcons';
+
+const styles = createStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: theme.spacing(2),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: style.$black
+  },
+  title: {
+    flexGrow: 1,
+  },
+  bar: {
+    boxShadow: '0px 2px 0px -1px rgba(0,0,0,0.09)'
+  },
+  drawerPaper: {
+    width: '100%'
+  }
+}));
+
+@inject('globalStore')
+@observer
+class Header extends React.Component{
+  @observable openMenu = false;
+
+  @action
+  toggleMenu = () => {
+    this.openMenu = !this.openMenu;
+  };
+
+  handleClick = () => {
+    console.log('menu')
+  };
+
+  menu = [
+    {
+      cat: 'Concerts',
+      page: 'page',
+      name: 'Концерты',
+      link: 'events/concerts',
+    }, {
+      cat: 'Festivals',
+      page: 'page',
+      name: 'Фестивали',
+      link: 'events/festivals',
+    }
+  ];
+
+  render() {
+    const { classes, globalStore:{ baseNameForRouting } } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="relative" className={classes.bar}>
+          <Toolbar>
+            <IconButton onClick={this.toggleMenu} className={`${classes.menuButton}`} aria-label="menu">
+              {BilegoIconMenuDotted}
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Bilego
+            </Typography>
+            <IconButton className={`${classes.menuButton}`} aria-label="search">
+              {BilegoIconSearch}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <SwipeableDrawer
+          classes={{paper: classes.drawerPaper}}
+          className={classes.drawer}
+          open={this.openMenu}
+          onClose={this.toggleMenu}
+          onOpen={this.toggleMenu}>
+          <AntMenu onSelect={this.handleClick} >
+            {this.menu.map(el=>(
+              <AntMenu.Item key={el.cat} cat={el.cat} page={el.page} name={el.name}>
+                <NavLink to={`/${baseNameForRouting}/${el.link}`} exact activeClassName="menu__item-select" className="menu__item">
+                  <Typography component="h6" variant="h6" className="menu__item-name">{el.name}</Typography>
+                </NavLink>
+              </AntMenu.Item>
+            ))}
+            <AntMenu.Divider />
+          </AntMenu>
+        </SwipeableDrawer>
+      </div>
+    )
+  }
 }
+
+export default withStyles(styles)(Header);
--- app/mobile/pages/FrontPage/index.js
+++ app/mobile/pages/FrontPage/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
 
-function FrontPage() {
-  return (<div>FrontPage</div>);
+import Typography from '@material-ui/core/Typography';
+import { Carousel } from 'antd';
+
+@inject('globalStore', 'sliderStore')
+@observer
+class FrontPage extends React.Component{
+  componentDidMount() {
+    const { globalStore:{ apiRoot }, sliderStore:{ getMainSlides }}  = this.props;
+    getMainSlides(apiRoot);
+  }
+
+  render() {
+    const { globalStore:{ baseNameForRouting }, sliderStore:{ slides } } = this.props;
+
+    console.log(slides)
+
+    return (
+      // {item.events.length > 0 && <Carousel effect="fade" autoplay>
+      //     {item.events.map(event => {
+      //       return (
+      //         <div key={event.id}>
+      //           <Gradient/>
+      //           <Image alt={event.title} img={event.origin_img}/>
+      //           <Link to={`/${baseNameForRouting}/event/${event.name}`} className="bilego-item-slider-event-title">
+      //             <Typography variant="subtitle2" component="div">
+      //               {event.title}
+      //             </Typography>
+      //           </Link>
+      //         </div>
+      //       )
+      //     })}
+      //   </Carousel>
+      // }
+      <div>1</div>
+    );
+  }
 }
 
 export default FrontPage;
